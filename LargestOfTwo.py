413ffbd19a6f813ce0e02260d41b8537a7e788fe
Sarthika252 <sarthikasuryawanshi@gmail.com>
2024-08-15 18:12:21 +0530

    commit

---
+++ LargestOfTwo.py
@@ -0,0 +1,8 @@
+a=float(input("Enter the first number:"))
+b=float(input("Enter the second number:"))
+if a>b:
+    print(" ",a,"is greater than ",b)
+elif b>a:
+    print(" ",b,"is greater than ",a)
+else:
+    print("Both are equal")
