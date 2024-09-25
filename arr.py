455aefbe7c4197fa29b667b18f6f8d4cdf5e7b25
Sarthika252 <sarthikasuryawanshi@gmail.com>
2024-09-25 15:40:37 +0530

    commit

---
+++ arr.py
@@ -0,0 +1,30 @@
+import array as arr
+a = arr.array('i', [1, 2, 3])
+print("The new created array is : ", end=" ")
+for i in range(0, 3):
+    print(a[i], end=" ")
+print()
+#Access elements of array
+print("Access element is: ", a[0])
+print("Access element is: ", a[1])
+
+
+#Pop and remove
+import array
+arr = array.array('i', [1, 2, 3, 1, 5])
+print("The new created array is : ", end="")
+for i in range(0, 5):
+    print(arr[i], end=" ")
+
+print("\r")
+print("The popped element is : ", end="")
+print(arr.pop(2))
+print("The array after popping is : ", end="")
+for i in range(0, 4):
+    print(arr[i], end=" ")
+
+print("\r")
+arr.remove(1)
+print("The array after removing is : ", end="")
+for i in range(0, 3):
+    print(arr[i], end=" ")
